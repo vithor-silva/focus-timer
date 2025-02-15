5fc9dd7be19aba0b68c8e69240b89d17ce7e15d8
Vithor <contatovithorsilva@gmail.com>
2025-02-15 11:06:50 -0300

    Cria a pagina History

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,7 +1,72 @@
+import { HistoryContainer, HistoryList } from "./styles";
+
 export function History() {
     return (
-        <div>
-            <h1>History</h1>
-        </div>
+        <HistoryContainer>
+            <h1>Meu histórico</h1>
+            <HistoryList>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Tarefa</th>
+                            <th>Duração</th>
+                            <th>Início</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>há 2 horas atrás</td>
+                            <td>Em andamento</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>há 2 horas atrás</td>
+                            <td>Em andamento</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>há 2 horas atrás</td>
+                            <td>Em andamento</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>há 2 horas atrás</td>
+                            <td>Em andamento</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>há 2 horas atrás</td>
+                            <td>Em andamento</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>há 2 horas atrás</td>
+                            <td>Em andamento</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>há 2 horas atrás</td>
+                            <td>Em andamento</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>há 2 horas atrás</td>
+                            <td>Em andamento</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </HistoryList>
+        </HistoryContainer>
+
     )
 }
