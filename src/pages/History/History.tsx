2b21055e877b9e701ffb530dd3ae05f88e5afdd7
Vithor <contatovithorsilva@gmail.com>
2025-02-17 08:59:24 -0300

    Adiciona funcionalidade aos status das tarefas do histórico.

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
     return (
@@ -19,49 +19,51 @@ export function History() {
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>há 2 horas atrás</td>
-                            <td>Em andamento</td>
+                            <td>
+                                <Status statusColor="green">
+                                    Concluido
+                                </Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>há 2 horas atrás</td>
-                            <td>Em andamento</td>
+                            <td>
+                                <Status statusColor="green">
+                                    Concluido
+                                </Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>há 2 horas atrás</td>
-                            <td>Em andamento</td>
+                            <td>
+                                <Status statusColor="yellow">
+                                    Em andamento
+                                </Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>há 2 horas atrás</td>
-                            <td>Em andamento</td>
+                            <td>
+                                <Status statusColor="green">
+                                    Concluido
+                                </Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>há 2 horas atrás</td>
-                            <td>Em andamento</td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>há 2 horas atrás</td>
-                            <td>Em andamento</td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>há 2 horas atrás</td>
-                            <td>Em andamento</td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>há 2 horas atrás</td>
-                            <td>Em andamento</td>
+                            <td>
+                                <Status statusColor="red">
+                                    Interrompido
+                                </Status>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
